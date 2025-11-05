642b8dffc722d1cdfee86a5e181c611b417ebd45
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-11-05 13:10:24 +0200

    Remove unnecessary whitespace in FieldWithErrorTextWidget for cleaner code.

--- lib/src/core/widgets/field_with_error_text_widget.dart
+++ lib/src/core/widgets/field_with_error_text_widget.dart
@@ -19,7 +19,6 @@ class FieldWithErrorTextWidget extends StatelessWidget {
       children: [
 
         fieldWidget,
-        
         ErrorTextWidget(
           errorText: errorText, 
           style: errorTextStyle,
